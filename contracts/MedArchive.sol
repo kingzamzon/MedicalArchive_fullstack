789264dd098c237c2b1b2d324f89b33d61c3f74d
Godand <aezeigwe2003@gmail.com>
2023-01-23 23:22:47 +0000

    add patient function

--- contracts/MedArchive.sol
+++ contracts/MedArchive.sol
@@ -6,19 +6,36 @@ contract MedArchive {
 
     address private s_owner;
 
-    struct FolderRecord {
-        uint256 id;
-        uint256 date;
-        string description;
-        string Cid;
-    }
+
+    // struct FolderRecord {
+    //     uint256 id;
+    //     uint256 date;
+    //     string description;
+    //     string Cid;
+    // }
 
     struct Folder {
-        uint256 PatientId;
-        string PatientName;
-        FolderRecord[] records;
+        uint256 patientId;
+        string patientName;
+        string[] recordsCid;
     }
+
+    mapping (address => uint256) private s_customerIds;
+    mapping (address =>mapping (uint256 => Folder[])) private s_hospitalRecords;
+
+    event PatientAdded(address indexed hospitalAddress,uint256 id);
+
     constructor() {
         s_owner=msg.sender;
+    }
+
+    function addPatient(string calldata _patientName) external {
+        uint256 id= s_customerIds[msg.sender]+=1;
+        s_hospitalRecords[msg.sender][id].push(Folder({
+            patientId:id,
+            patientName:_patientName,
+            recordsCid:new string[](0)
+            }));
+        emit PatientAdded(msg.sender,id);
     }
 }
