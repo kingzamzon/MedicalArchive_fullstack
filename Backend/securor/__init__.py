f3350b5f2e193963d8b3425f54fe80d130dbf167
Godand <aezeigwe2003@gmail.com>
2023-01-25 14:52:11 +0000

    tests, password hashing

--- Backend/securor/__init__.py
+++ Backend/securor/__init__.py
@@ -5,83 +5,102 @@ import os
 
 
 def create_app(test_config=None):
-    app=Flask(__name__)
+    app = Flask(__name__)
     CORS(app)
 
-    KEY= os.getenv("FERNET_KEY")
-    f=Fernet(bytes(KEY,'utf-8'))
+    KEY = os.getenv("FERNET_KEY")
+    # f = Fernet(bytes(KEY, "utf-8"))
 
     @app.after_request
     def after_request(response):
         response.headers.add(
             "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
         )
-        response.headers.add(
-            "Access-Control-Allow-Methods", "GET"
-        )
-        response.headers.add(
-            "Access-Control-Allow-Origin", "*"
-        )
+        response.headers.add("Access-Control-Allow-Methods", "GET")
+        response.headers.add("Access-Control-Allow-Origin", "*")
         return response
-    
-    def decrypt(data):
-        result= bytes(data,'utf-8')
-        return f.decrypt(result).decode()
 
-    @app.route("/encode",methods=["GET"])
+    def decrypt(data,password=""):
+        result = bytes(data, "utf-8")
+        return Fernet(bytes(KEY + password, "utf-8")).decrypt(result).decode()
+
+    @app.route("/encode", methods=["GET"])
     def encrypt_data():
-        data=request.get_json()
+        data = request.get_json()
 
-        cid=data.get("cid")
-        return jsonify({
-            "status":200,
-            "success":True,
-            "hash":f.encrypt(cid.encode())
-        }),200
+        cid = data.get("cid")
+        password = data.get("password")
+        return (
+            jsonify(
+                {
+                    "status": 200,
+                    "success": True,
+                    "hash": Fernet(bytes(KEY + password, "utf-8")).encrypt(cid.encode()).decode(),
+                }
+            ),
+            200
+        )
 
-    @app.route("/decode",methods=["GET"])
+    @app.route("/decode", methods=["GET"])
     def decrypt_data():
-        data=request.get_json()
-        cids= data.get_list("cids").split(",")
-        hashes=map(decrypt, cids)
-        return jsonify({
-            "status":200,
-            "success":True,
-            "cids":hashes
-        }),200
+        data = request.get_json()
+        cids = data["cids"]
+        password = data["password"] 
+        hashes = map(lambda ids: decrypt(ids,password=password), cids)
+        return jsonify(
+                {
+                "status": 200,
+                "success": True, 
+                "cids": list(hashes)
+                }
+            ), 200
 
     @app.errorhandler(405)
     def method_not_allowed(error):
-        return jsonify({
-            "status": 405,
-            "message": "Method is not allowed",
-            "success": False
-        }), 405
+        return (
+            jsonify(
+                {
+                    "status": 405, 
+                    "message": "Method is not allowed", 
+                    "success": False
+                }
+            ),
+            405
+        )
 
     @app.errorhandler(422)
     def cant_process(error):
-        return jsonify({
-            "status": 422,
-            "success": False,
-            "message": "Request unprocessable"
-        }), 422
+        return (
+            jsonify(
+                {
+                    "status": 422, 
+                    "success": False, 
+                    "message": "Request unprocessable"
+                }
+            ),
+            422
+        )
 
     @app.errorhandler(400)
     def bad_request(error):
-        return jsonify({
-            "status": 400,
-            "success": False,
-            "message": "Bad Request"
-            
-        }), 400
+        return jsonify(
+            {
+                "status": 400, 
+                "success": False, 
+                "message": "Bad Request"
+            }), 400
 
     @app.errorhandler(500)
     def server_error(error):
-        return jsonify({
-            "status": 500,
-            "success": False,
-            "message": "Internal server error"
-        }), 500
+        return (
+            jsonify(
+                {
+                    "status": 500, 
+                    "success": False, 
+                    "message": "Internal server error"
+                }
+            ),
+            500
+        )
 
     return app
-
---
+++ Backend/test_securor.py
@@ -0,0 +1,66 @@
+import os
+import unittest
+import json
+from securor import create_app
+
+
+class SecurorTestCase(unittest.TestCase):
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    def test_encode(self):
+        """should return a valid hash in string"""
+        res = self.client().get(
+            "/encode", json={"cid": "vgcwhjcjdw jenywdvcjwe76325767hou3bdtfi3db","password":"test"}
+        )
+        data = json.loads(res.data)
+        self.assertTrue(isinstance(data["hash"], str))
+        self.assertTrue(res.status_code == 200)
+
+    def test_decode(self):
+        """should return same cid or data in string"""
+        res = self.client().get(
+            "/encode", json={"cid": "vgcwhjcjdw jenywdvcjwe76325767hou3bdtfi3db","password":"test"}
+        )
+        data = json.loads(res.data)["hash"]
+        res = self.client().get(
+            "/decode",
+            json={
+                "cids": [
+                    data
+                ],
+                "password":"test"
+            },
+        )
+        data = json.loads(res.data)["cids"]
+        self.assertEqual(data[0], "vgcwhjcjdw jenywdvcjwe76325767hou3bdtfi3db")
+        self.assertTrue(isinstance(data[0], str) and isinstance(data, list))
+
+    def test_not_give_correct_cid_with_wrong_password(self):
+        "if wrong or different password is given you should not get same cid"
+        res = self.client().get(
+            "/encode", json={"cid": "vgcwhjcjdw jenywdvcjwe76325767hou3bdtfi3db","password":"test"}
+        )
+        data= json.loads(res.data)["hash"]
+        res = self.client().get(
+            "/decode",
+            json={
+                "cids": [
+                    data
+                ],
+                "password":"wrong password"
+            },
+        )
+        data= json.loads(res.data)["cids"]
+        self.assertNotEqual(data[0],"vgcwhjcjdw jenywdvcjwe76325767hou3bdtfi3db")
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
